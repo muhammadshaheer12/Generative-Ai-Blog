8b9d8d6c842a5afb924611c03fcac60afdbf6c27
Muhammad Shaheer <159147097+muhammadshaheer12@users.noreply.github.com>
2025-01-08 05:23:28 +0500

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <footer className="bg-gradient-to-r from-purple-300 via-purple-500 to-purple-900 py-4 sm:py-6 md:py-8 text-center text-black">
         <p className="text-sm sm:text-lg md:text-xl lg:text-2xl">
-          &copy; 2025 All Rights Reserved.
+          &copy; 2025 All rights reserved.
         </p>
       </footer>
     );
